71a49ca803f003c8d55bbfb3751384a7c449ce09
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2023-02-23 23:21:59 +0100

    add docker-compose for postgresql db, change application.properties (problem with start application without SpringApplicationBuilder)

--- src/main/java/cz/cvut/fel/constructa/ConstructaApplication.java
+++ src/main/java/cz/cvut/fel/constructa/ConstructaApplication.java
@@ -2,18 +2,19 @@ package cz.cvut.fel.constructa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ConstructaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConstructaApplication.class, args);
-    }
-
 //    public static void main(String[] args) {
-//        SpringApplicationBuilder app = new SpringApplicationBuilder(DemoApplication.class)
-//                .properties("spring.config.location=classpath:application.properties,classpath:application.properties");
-//        app.run(args);
+//        SpringApplication.run(ConstructaApplication.class, args);
 //    }
+
+    public static void main(String[] args) {
+        SpringApplicationBuilder app = new SpringApplicationBuilder(ConstructaApplication.class)
+                .properties("spring.config.location=classpath:application.properties,classpath:application.properties");
+        app.run(args);
+    }
 
 }
